d0e5906abcf4adc116e896f6c258510dc3bf6dfc
Korben <46075658+korbendallas01@users.noreply.github.com>
2018-12-23 09:52:32 +0100

    Initial upload

---
+++ README.md
@@ -0,0 +1,29 @@
+Font Loader plugin for Koken
+=================================
+
+This [Koken](http://koken.me) plugin allows you to quickly and easily integrate web fonts from a variety of providers (Typekit, Google, Fontdeck, Fonts.com). Koken 0.5+ required.
+
+Documentation: http://help.koken.me/customer/portal/articles/1015639-font-loader
+
+License
+-------
+
+Copyright (c) 2013-6 Koken.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ plugin.php
@@ -0,0 +1,74 @@
+<?php
+
+class KokenFontLoader extends KokenPlugin {
+
+	function __construct()
+	{
+		$this->require_setup = true;
+		$this->register_hook('before_closing_head', 'render');
+	}
+
+	function render()
+	{
+
+		$output = (object) array();
+
+		if (!empty($this->data->typekit)) {
+			$output->typekit = (object) array('id' => $this->data->typekit);
+		}
+
+		if (!empty($this->data->google)) {
+			$fonts = explode(',', $this->data->google);
+			$families = array();
+			$weights = false;
+			foreach($fonts as $f) {
+				if (strpos($f, '(') !== FALSE) {
+					$weights = true;
+					$parts = explode('(', $f);
+					$family = trim($parts[0]) . ':400,' . trim($parts[1]);
+					if (strpos($f, ')') !== FALSE) {
+						$family = trim(str_replace(')', '', $family));
+						$weights = false;
+					}
+				}
+				if ($weights) {
+					if (strpos($family, ':400') !== FALSE && !strpos($f, '(') && !strpos($f, ')')) {
+						$family .= ',' . trim($f);
+					}
+					if (strpos($f, ')') !== FALSE) {
+						if (!strpos($family, ':')) {
+							$family .= ':';
+						} else {
+							$family .= ',';
+						}
+						$family .= str_replace(')', '', trim($f));
+						$weights = false;
+					}
+				}
+				if (!$weights) {
+					array_push($families, (isset($family)) ? trim($family) : trim($f));
+					unset($family);
+				}
+			}
+			$output->google = (object) array('families' => $families);
+		}
+
+		if (!empty($this->data->fontdeck)) {
+			$output->fontdeck = (object) array('id' => $this->data->fontdeck);
+		}
+
+		if (!empty($this->data->fontscom)) {
+			$output->monotype = (object) array('projectId' => $this->data->fontscom);
+		}
+
+		$output = json_encode($output);
+		$fallback = 'document.write(\'<script src="'.$this->get_path().'/webfont.js">\x3C/script>\')';
+
+		echo <<<OUT
+<script src="//ajax.googleapis.com/ajax/libs/webfont/1/webfont.js"></script>
+<script>window.WebFont || $fallback</script>
+<script type="text/javascript">WebFont.load($output);</script>
+OUT;
+
+	}
+}
